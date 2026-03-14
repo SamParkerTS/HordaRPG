753fcfe41445dca88646e00aca96fb8d9e7f696b
SamParkerTS <samukvieira00@gmail.com>
2026-03-14 16:18:13 -0300

    melhorias no app.py

--- src/core/app.py
+++ src/core/app.py
@@ -1,17 +1,20 @@
 import sys
-from core.logger import Logger
-from core.config import Config
 
 from PySide6.QtWidgets import QApplication
 
 from core.paths import Paths
-from ui.main_window import MainWindow
+from core.config import Config
+from core.logger import Logger
 from core.file_manager import FileManager
+
+from ui.main_window import MainWindow
+
 
 class Application:
 
     def __init__(self):
 
+        # Inicialização do núcleo
         self.paths = Paths()
 
         self.config = Config(self.paths)
@@ -20,15 +23,33 @@ class Application:
 
         self.file_manager = FileManager(self.paths, self.logger)
 
+        # Inicialização da interface
         self.qt_app = QApplication(sys.argv)
 
         self.main_window = MainWindow()
 
+        self.logger.info("Aplicação inicializada.")
+
     def run(self):
+
         """
-        Inicia a interface e o loop da aplicação.
+        Executa a aplicação.
         """
+
+        self.logger.info("Aplicação iniciada.")
 
         self.main_window.show()
 
-        return self.qt_app.exec()
+        exit_code = self.qt_app.exec()
+
+        self.shutdown()
+
+        sys.exit(exit_code)
+
+    def shutdown(self):
+
+        """
+        Encerramento seguro da aplicação.
+        """
+
+        self.logger.info("Encerrando aplicação.")
